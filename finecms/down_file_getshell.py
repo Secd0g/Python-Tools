1bf9dad4cd7ee56ecb5936f90ae61a6a40420371
F0rmat <F0r3at@users.noreply.github.com>
2018-03-01 02:28:36 +0800

    Update down_file_getshell.py

--- finecms/down_file_getshell.py
+++ finecms/down_file_getshell.py
@@ -1,6 +1,6 @@
 '''
 author:F0rmat
-usage:
+usage:http://getpass.cn/2018/02/28/%e9%80%9a%e6%9d%80FineCMS5.0.8%e5%8f%8a%e7%89%88%e6%9c%ac%e4%bb%a5%e4%b8%8bgetshell%e7%9a%84%e6%bc%8f%e6%b4%9e(%e6%af%8f%e5%a4%a9%e4%b8%80%e6%b4%9e)
 '''
 import sys
 import random
